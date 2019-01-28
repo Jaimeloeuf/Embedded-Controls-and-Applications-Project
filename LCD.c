5a128c9fdd185c5647b28d91d167a867a8303026
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-28 11:56:48 +0800

    Changes: 1.	Added a switcher to the LCD function and added the write method to the 	LCD using the 4Bit databus interface.
    2.	Updated the Init_LCD function to set the CONN_TYPE variable.
    
    3.	Updated all names of the menus in the menu source file to represent the
    	ID created in the menu display function. And testing a new way to display
    	with the abv setup.

--- LCD.c
+++ LCD.c
@@ -12,34 +12,80 @@
 // Global variable to maintain state off the databus connection type. 1 for 8 Bit connection, 0 for 4 Bit connection.
 uint8_t CONN_TYPE;
 
+const char MESS[] = "ECAPP LAB 3";
+void example_code(void)
+{
+	// Initialise LCD to use 4-bit databus interface, with multi-line display
+	Init_LCD(4);
+
+	for (i = 0; MESS[i]; i++) // Output message to LCD
+		W_data_4bit(MESS[i]); // Write individual character to LCD
+
+	// Below is example code on using the LCD
+	W_ctr_4bit(0xc0);		  // Set cursor to line 2
+	itoa(buff, 12, 10);		  // function itoa is used to convert integer stored at result to ASCII and keep to MESS
+	for (i = 0; buff[i]; i++) // Write data to LCD up to null
+		W_data_4bit(buff[i]); // Write character to LCD
+}
+
 void LCD(uint8_t cls, uint8_t line, char *msg)
 {
 	// Have a check for the CONNECTION TYPE to determine which functions to use.
-
-	Init_LCD(); // Initialize LCD to 8-bit interface with multiple line
-	if (cls)
-		LCD_sendCW(0b00000001); // Clear the display
-	switch (line)
+	switch (CONN_TYPE)
 	{
-	case LINE1:
-		LCD_sendCW(0b00000010);
-		break; // Return cursor to LINE1 Home position
-	case LINE2:
-		LCD_sendCW(0b11000000);
-		break; // Return cursor to LINE2 Home position
+	case 0:
+		if (cls)
+			W_ctr_4bit(1); // Clear the display & goto home position
+		switch (line)
+		{
+		case LINE1:
+			W_ctr_4bit(0x80); // Set cursor to line 1
+			break;
+		case LINE2:
+			W_ctr_4bit(0xc0); // Set cursor to line 2
+			break;
+		}
+		for (uint8_t i = 0; *(msg + i) != 0; i++)
+			LCD_sendData(*(msg + i));
+
+		break;
+	case 1:
+		if (cls)
+			LCD_sendCW(0b00000001); // Clear the display
+		switch (line)
+		{
+		case LINE1:
+			LCD_sendCW(0b00000010);
+			break; // Return cursor to LINE1 Home position
+		case LINE2:
+			LCD_sendCW(0b11000000);
+			break; // Return cursor to LINE2 Home position
+		}
+		for (uint8_t i = 0; *(msg + i) != 0; i++)
+			LCD_sendData(*(msg + i));
+		break;
+
+	default:
+		return 0; // Return 0 to indicate failure
 	}
-	for (uint8_t i = 0; *(msg + i) != 0; i++)
-		LCD_sendData(*(msg + i));
+	return 1; // Return 1 to indicate success.
 }
+
 uint8_t Init_LCD(uint8_t data_bus_width)
 {
 	// User can choose to either use the 8Bit or the 4Bit databus version to connect to the LCD
 	switch (data_bus_width)
 	{
 	case 4:
+		// Set the variable that keeps track of the connection type
+		CONN_TYPE = 0;
+		// Initialise the LCD
 		Init4bit();
 		break;
 	case 8:
+		// Set the variable that keeps track of the connection type
+		CONN_TYPE = 1;
+		// Initialise the LCD
 		Init8bit();
 		break;
 	// Onlt 4 or 8 are valid inputs. If anything else, will return a 0 for false/failed.
@@ -49,23 +95,13 @@ uint8_t Init_LCD(uint8_t data_bus_width)
 	// Return 1 for true/operation-success
 	return 1;
 }
+
+// Function to Initialize LCD to 8-bit interface with multiple line
 void Init8bit()
 {							// Initializes the LCD
 	LCD_sendCW(0b00111000); // Function Set - 8-bit, 2 lines, 5X7
 	LCD_sendCW(0b00001100); // Display on, cursor on
 	LCD_sendCW(0b00000110); // Entry mode - inc addr, no shift
-}
-void Init4bit()
-{ /* LCD display initialization */
-	// Special Sequence a) to d) required for 4-bit interface
-	_delay(15);				// a) 15ms LCD power-up delay
-	W_ctr_4bit(0x03);		// b) Function Set (DB4-DB7: 8-bit interface)
-	_delay(5);				// c) 5ms delay
-	W_ctr_4bit(0x02);		// d) Function Set (DB4-DB7: 4-bit interface)
-	W_ctr_4bit(0b00101000); // Function Set - 4-bit, 2 lines, 5X7
-	W_ctr_4bit(0b00001100); // Display on, cursor off
-	W_ctr_4bit(0b00000110); // Entry mode - inc addr, no shift
-	W_ctr_4bit(0b00000001); // Clear display & home position
 }
 
 void LCD_sendCW(char x)
@@ -85,26 +121,18 @@ void LCD_sendData(char x)
 	_delay(500); // 1ms delay
 }
 
-void W_ctr_4bit(char);  // 4-bit Control word for LCD
-void W_data_4bit(char); // 4-bit Text Data for LCD
-
-
-
-
-const char MESS[] = "ECAPP LAB 3";
-void example_code(void)
+// Function to Initialize LCD to 4-bit data interface with multiple line
+void Init4bit()
 {
-	// Initialise LCD to use 4-bit databus interface, with multi-line display
-	Init_LCD(4);
-	for (i = 0; MESS[i]; i++) // Output message to LCD
-		W_data_4bit(MESS[i]); // Write individual character to LCD
-
-
-	// Below is example code on using the LCD
-	W_ctr_4bit(0xc0);		  // Set cursor to line 2
-	itoa(buff, 12, 10);		  // function itoa is used to convert integer stored at result to ASCII and keep to MESS
-	for (i = 0; buff[i]; i++) // Write data to LCD up to null
-		W_data_4bit(buff[i]); // Write character to LCD
+	// Special Sequence a) to d) required for 4-bit interface
+	_delay(15);				// a) 15ms LCD power-up delay
+	W_ctr_4bit(0x03);		// b) Function Set (DB4-DB7: 8-bit interface)
+	_delay(5);				// c) 5ms delay
+	W_ctr_4bit(0x02);		// d) Function Set (DB4-DB7: 4-bit interface)
+	W_ctr_4bit(0b00101000); // Function Set - 4-bit, 2 lines, 5X7
+	W_ctr_4bit(0b00001100); // Display on, cursor off
+	W_ctr_4bit(0b00000110); // Entry mode - inc addr, no shift
+	W_ctr_4bit(0b00000001); // Clear display & home position
 }
 
 void W_ctr_4bit(char x)
--- menu.c
+++ menu.c
@@ -6,46 +6,55 @@
 
 #include <stdint.h>
 #include <xc.h>
-
-// char *strs[NUMBER_OF_STRINGS] = {"foo", "bar", "bletch", ...};
+#include "LCD.h"
 
 // An array of pointers to constant char strings.
-// char *menu[NUMBER_OF_STRINGS] = {
-// 	"View current
-// 	temperature",
-// 	"bar",
-// 	"bletch"};
-
-char *menu1[NUMBER_OF_STRINGS] = {
+char *menu0[NUMBER_OF_STRINGS] = {
 	"Menu (5)",
 	"s3:back s2:next"};
 
-char *menu1[NUMBER_OF_STRINGS] = {
+char *menu10[NUMBER_OF_STRINGS] = {
 	"View current",
 	"temperature"};
 
-char *menu2[] = {
+char *menu20[] = {
 	"Toggle operating",
 	"mode"};
 
-char *menu3[] = {
+char *menu21[] = {
+	"Current Mode:",
+	""}; // Second line to display the current mode as either man or auto, after the mode toggle
+
+char *menu30[] = {
 	"View current",
 	"temp. threshold"
 };
 
-char *menu3[] = {
+char *menu31[] = {
+	"Current temp.",
+	""}; // Second line to display the reading that was taken immediately after calling menu30
+
+char *menu40[] = {
 	"Set new temp.",
 	"threshold"
 };
 
-char *menu[] = {
+char *menu41[] = {
 	"Enter new temp.",
-	""
+	"" // Second line is to display the user input.
 };
 
-char *menu[] = {
-	"Set sprinkler strength"
+char *menu50[] = {
+	"Set sprinkler",
+	"strength"
 };
+
+char *menu51[] = {
+	"Percent of Max:",
+	"" // Second line is to display the user input.
+};
+
+// Every single menu should have a corresponding function to deal with it.
 
 
 /*
@@ -58,37 +67,26 @@ Press a button to enter
 Press E to exit
 Press D to show a decimal point if allowed (Flash invalid for a second.)
 */
-char *menu[] = {
-	"Enter strength",
-	"as a percentage"
-};
 
-char **menu[Number of lines to be stored] = {
-	menu1,
-	menu2,
-	menu3,
-	menu4};
+
+/* To make the input use variable arguements and default to 0 */
 
 // Use the below function to choose what to display on the LCD.
 unsigned char menu_disp(uint8_t menu_item, uint8_t sub_menu_item)
 {
 	// Get the static part of the message to display from the 2D array
 	// message = menu[menu_item][sub_menu_item];
+	// get the menu ID
+	menuID = (menu_item * 10) + sub_menu_item;
 
-	// Get the pointer to the current menu content
-	current_menu = menu[menu_item];
-	// Get the pointer to the current sub menu content.
-	current_menu = current_menu[sub_menu_item];
-
-	int *arrop[3];
-	int a = 10, b = 20, c = 50, i;
-
-	arrop[0] = &a;
-	arrop[1] = &b;
-	arrop[2] = &c;
-
-	for (i = 0; i < 3; i++)
+	switch (menuID)
 	{
-		printf("Address = %d\t Value = %d\n", arrop[i], *arrop[i]);
+		case 0:
+			menu0[0];
+			menu0[1];
+			break;
+	
+		default:
+			break;
 	}
 }
